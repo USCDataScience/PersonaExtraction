3603ff8f1d3a00ff75fabeb35f83daddceff37a3
Chris Mattmann <mattmann@apache.org>
2016-04-25 18:51:31 -0400

    - formatting and cleanup

--- src/main/java/edu/usc/ir/UserExtractor.java
+++ src/main/java/edu/usc/ir/UserExtractor.java
@@ -42,147 +42,111 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-
 public class UserExtractor {
 
-	
   private static HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
-  // public static void main(String[] args) throws
-  // FailingHttpStatusCodeException, MalformedURLException, IOException {
 
-	@SuppressWarnings("deprecation")
-	public HashMap<String, ArrayList<String>> persons(String host) throws FailingHttpStatusCodeException, MalformedURLException, IOException, SolrServerException {
-		
-		
-		Properties prop = new Properties();
-	    InputStream input = null;
-	    input = UserExtractor.class.getClassLoader().getResourceAsStream("config.properties");
+  @SuppressWarnings("deprecation")
+  public HashMap<String, ArrayList<String>> persons(String host)
+      throws FailingHttpStatusCodeException, MalformedURLException, IOException,
+      SolrServerException {
 
-        // load a properties file
-        prop.load(input);
+    Properties prop = new Properties();
+    InputStream input = null;
+    input = UserExtractor.class.getClassLoader()
+        .getResourceAsStream("config.properties");
 
-        // get the property value and print it out
-        String username_server=prop.getProperty("username");
-        String password_server=prop.getProperty("password");
-	
-        String pattern = prop.getProperty(host);
-        String domain = host.split("\\.")[1];
-        System.out.println(host+username_server+password_server);
-        System.out.println(pattern);
-		WebClient webclient = new WebClient();
-		DefaultCredentialsProvider provider = new DefaultCredentialsProvider();
-		provider.addCredentials(username_server, password_server);
-		webclient.setCredentialsProvider(provider);
-		
-		webclient.getOptions().setJavaScriptEnabled(false);
-		
-		webclient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-		webclient.getOptions().setThrowExceptionOnScriptError(false);
-		
-		
-		String urlString = "http://imagecat.dyndns.org/solr/imagecatdev";
-		HttpSolrServer server = new HttpSolrServer(urlString);
-		
-		HttpClientUtil.setBasicAuth((DefaultHttpClient) server.getHttpClient(), username_server, password_server);
-		
-		SolrQuery query = new SolrQuery();
-		query.setQuery("host:"+host);
-		query.set("fl", "id,persons");
-		query.setStart(0);
-		query.setRows(10);
-		QueryResponse response = server.query(query);
-		//System.out.println(response);
-		SolrDocumentList docResults = response.getResults();
-		//System.out.println(docResults);
-		List<String> htmldocs = new ArrayList<String>();
-		for(int i=0;i<docResults.size();i++)
-		{
-		String id = docResults.get(i).getFieldValue("id").toString();
-		
-		int index=id.lastIndexOf('/');
-  	   String lastString=(id.substring(index +1));
-		
-		htmldocs.add(lastString);
-		}		
-		
-		for(int i=0;i<htmldocs.size();i++)
-		{
-			System.out.println(htmldocs.get(i));
-		}
-			
-		
-		for(int j=0;j<htmldocs.size();j++)
-		{
-			try
-			{
-				
-			
-			ArrayList<String> username = new ArrayList<String>();
-		
-		String url = "http://imagecat.dyndns.org/weapons/alldata/com/"+domain+"/www/"+htmldocs.get(j);
-		System.out.println(j+"\t"+url);
-		HtmlPage htmlPage = webclient.getPage(url);
-		
-		
-		@SuppressWarnings("unchecked")
-		
-	//	List<HtmlAnchor> anchor = (List<HtmlAnchor>) htmlPage.getByXPath("//*[contains(@href,'members')]");
-		List<HtmlAnchor> anchor = (List<HtmlAnchor>) htmlPage.getByXPath(pattern);
-		//List<HtmlAnchor> anchor = (List<HtmlAnchor>) htmlPage.getByXPath("//')]");
-		for(int i=0;i<anchor.size();i++)
-		{
-			
-		//	System.out.println(anchor.get(i).getHrefAttribute());
-			String link = anchor.get(i).getHrefAttribute();
-			if(link.toLowerCase().contains("php"))
-			{
-				continue;
-			}
-			else if(link.toLowerCase().contains("?"))
-			{
-				continue;
-			}
-			else
-			{
-			int index=link.lastIndexOf('/');
-     	   String lastString=(link.substring(index +1));
-     	//   System.out.println(lastString);
-     	   username.add(lastString);
-			}
-		}
-		
-		
-		
-		LinkedHashSet<String> users = new LinkedHashSet<String>();
-	       users.addAll(username);
-	       username.clear();
-	       username.addAll(users);
-	   
-	       
-	       
-	       map.put(htmldocs.get(j), (ArrayList<String>) username);
-			}
-			catch(ClassCastException c)
-			{
-				continue;
-			}
-			catch (SocketTimeoutException e) {
-				// TODO: handle exception
-				continue;
-			}
-			catch (HttpHostConnectException e) {
-				// TODO: handle exception
-				continue;
-			}
-			catch (Exception e) {
-				// TODO: handle exception
-				continue;
-			}
-		}
-		
-		webclient.closeAllWindows();
-		
-		return map;	
-		
-	}
+    // load a properties file
+    prop.load(input);
+
+    // get the property value and print it out
+    String username_server = prop.getProperty("username");
+    String password_server = prop.getProperty("password");
+
+    String pattern = prop.getProperty(host);
+    String domain = host.split("\\.")[1];
+    System.out.println(host + username_server + password_server);
+    System.out.println(pattern);
+    WebClient webclient = new WebClient();
+    DefaultCredentialsProvider provider = new DefaultCredentialsProvider();
+    provider.addCredentials(username_server, password_server);
+    webclient.setCredentialsProvider(provider);
+
+    webclient.getOptions().setJavaScriptEnabled(false);
+
+    webclient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+    webclient.getOptions().setThrowExceptionOnScriptError(false);
+
+    String urlString = "http://imagecat.dyndns.org/solr/imagecatdev";
+    HttpSolrServer server = new HttpSolrServer(urlString);
+
+    HttpClientUtil.setBasicAuth((DefaultHttpClient) server.getHttpClient(),
+        username_server, password_server);
+
+    SolrQuery query = new SolrQuery();
+    query.setQuery("host:" + host);
+    query.set("fl", "id,persons");
+    query.setStart(0);
+    query.setRows(10);
+    QueryResponse response = server.query(query);
+    SolrDocumentList docResults = response.getResults();
+    List<String> htmldocs = new ArrayList<String>();
+    for (int i = 0; i < docResults.size(); i++) {
+      String id = docResults.get(i).getFieldValue("id").toString();
+
+      int index = id.lastIndexOf('/');
+      String lastString = (id.substring(index + 1));
+
+      htmldocs.add(lastString);
+    }
+
+    for (int i = 0; i < htmldocs.size(); i++) {
+      System.out.println(htmldocs.get(i));
+    }
+
+    for (int j = 0; j < htmldocs.size(); j++) {
+      try {
+
+        ArrayList<String> username = new ArrayList<String>();
+
+        String url = "http://imagecat.dyndns.org/weapons/alldata/com/" + domain
+            + "/www/" + htmldocs.get(j);
+        System.out.println(j + "\t" + url);
+        HtmlPage htmlPage = webclient.getPage(url);
+
+        @SuppressWarnings("unchecked")
+        List<HtmlAnchor> anchor = (List<HtmlAnchor>) htmlPage
+            .getByXPath(pattern);
+        for (int i = 0; i < anchor.size(); i++) {
+
+          // System.out.println(anchor.get(i).getHrefAttribute());
+          String link = anchor.get(i).getHrefAttribute();
+          if (link.toLowerCase().contains("php")) {
+            continue;
+          } else if (link.toLowerCase().contains("?")) {
+            continue;
+          } else {
+            int index = link.lastIndexOf('/');
+            String lastString = (link.substring(index + 1));
+            username.add(lastString);
+          }
+        }
+
+        LinkedHashSet<String> users = new LinkedHashSet<String>();
+        users.addAll(username);
+        username.clear();
+        username.addAll(users);
+
+        map.put(htmldocs.get(j), (ArrayList<String>) username);
+      } catch (Exception e) {
+        e.printStackTrace();
+        continue;
+      }
+    }
+
+    webclient.closeAllWindows();
+
+    return map;
+
+  }
 }
